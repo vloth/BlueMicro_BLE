c36d203c68e1d7aa82a7d9bdf9ea142f24295ccb
Pierre <jpconstantineau@gmail.com>
2020-08-10 21:16:25 -0600

    troubleshooting action

--- build/docker/gh-nrf52840.sh
+++ build/docker/gh-nrf52840.sh
@@ -27,8 +27,6 @@ arduinoPath="/usr/share/arduino"
 #arduinoAvrPath="$arduinoPath/hardware/arduino/avr"
 
 blueMicroPath=$(pwd)/../..
-ls ${blueMicroPath}
-ls /github
 firmwarePath="${blueMicroPath}/firmware"
 outputPath="${blueMicroPath}/output"
 outputTempPath="/tmp"
@@ -71,8 +69,8 @@ arduino_compile() {
    #sleep 2
 
    #Compile
-   cmdCompile832="arduino-cli compile -v --fqbn adafruit:nrf52:feather52832 --build-path $buildPath --build-cache-path $buildCachePath $sourcePath/firmware.ino  -o $buildPath/firmware"
-   cmdCompile840="arduino-cli compile -v --fqbn adafruit:nrf52:pca10056:softdevice=s140v6,debug=l0 --build-path $buildPath --build-cache-path $buildCachePath $sourcePath/firmware.ino  -o $buildPath/firmware"
+   cmdCompile832="/usr/local/bin/arduino-cli compile -v --fqbn adafruit:nrf52:feather52832 --build-path $buildPath --build-cache-path $buildCachePath $sourcePath/firmware.ino  -o $buildPath/firmware"
+   cmdCompile840="/usr/local/bin/arduino-cli compile -v --fqbn adafruit:nrf52:pca10056:softdevice=s140v6,debug=l0 --build-path $buildPath --build-cache-path $buildCachePath $sourcePath/firmware.ino  -o $buildPath/firmware"
 
    if $verbose; then 
       $cmdCompile840
